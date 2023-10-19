73ba0d06800a120e4d860f1dce93a025e903ee15
lvlivingston <lvlivingston@gmail.com>
2023-10-18 20:58:14 -0500

    completed homework with bonus

--- routes/skills.js
+++ routes/skills.js
@@ -4,7 +4,17 @@ var skillsCtrl = require('../controllers/skills');
 
 // GET /skills
 router.get('/', skillsCtrl.index);
+// GET /skills/new
+router.get('/new', skillsCtrl.new);
 // GET /skills/:id
 router.get('/:id', skillsCtrl.show);
+// GET /skills/:id/edit
+router.get('/:id/edit', skillsCtrl.edit);
+// POST /skills
+router.post('/', skillsCtrl.create);
+// DELETE /skills/:id
+router.delete('/:id', skillsCtrl.delete);
+// PUT /skills/:id
+router.put('/:id', skillsCtrl.update);
 
 module.exports = router;
